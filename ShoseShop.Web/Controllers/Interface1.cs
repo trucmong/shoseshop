f314280005ed1ab8e6853b6a967e2268c8f6253f
MONGPC\mongc <huynhanhtrucmong4104@gmail.com>
2022-09-16 08:02:16 +0700

    add interface

---
+++ ShoseShop.Web/Controllers/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoseShop.Web.Controllers
+{
+    internal interface Interface1
+    {
+    }
+}
